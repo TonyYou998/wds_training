f4e3a0609e5e445dbf0fbdfd32e59d743ac226da
tony-you-sout <tanvuu998@gmail.com>
2022-12-31 21:29:33 +0700

    add user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,11 +2,20 @@ const fs=require('fs');
 const data=fs.readFileSync("./models/db.json");
 const userDb=JSON.parse(data);
 const getAllUser=(req,res)=>{
-        console.log(userDb);
+       
         res.status(200).json(userDb.users);
+
+}
+const addUser=(req,res)=>{
+        const{id,name,age}=req.body;
+        const newUSer={id,name,age};
+        userDb.users=[...userDb.users,newUSer];
+        fs.writeFileSync('./models/db.json',JSON.stringify(userDb));
+        res.status(200).json("success");
 
 }
 
 module.exports={
     getAllUser,
+    addUser
 }
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,9 @@
 const express=require('express');
-const { getAllUser } = require('../controllers/user.controller');
+const { getAllUser, addUser } = require('../controllers/user.controller');
 const userRouter=express.Router();
 
 userRouter.get("/get-users",getAllUser);
+userRouter.post("/add-user",addUser);
 
 module.exports={
     userRouter,
